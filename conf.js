69c37951d5df424edf35c15fb541eff856df3e25
Lu Cho-Ching <choching.work@gmai.com>
2015-06-23 15:25:17 +0800

    可以build pages folder內容: Bookmarks和About

--- conf.js
+++ conf.js
@@ -2,5 +2,6 @@ module.exports = {
   name: "Cho-Ching's Blog",
   desc: '既然走過,就留下痕跡。',
   articleSource: './contents/articles/',
+  pageSource: './contents/pages/',
   buildto: './build/'
 };
--- gen.js
+++ gen.js
@@ -14,3 +14,20 @@ fs.readdirAsync(conf.articleSource)
   .done(function(){
     console.log('[done] index.html created.');
   });
+
+fs.readdirAsync('./contents/pages')
+  .map(function(fileName){
+    var title = fileName.split('.')[0];
+    return {
+      fileName: fileName,
+      headTitle: conf.name + ' - ' + title,
+      inPath: conf.pageSource + fileName,
+      outPath: conf.buildto + 'pages/'+ title + '.html',
+      content: fs.readFileAsync(conf.pageSource + fileName, 'utf8')
+    };
+    })
+  .each(utils.markdownToHtml)
+  .done(function(){
+    console.log('[done] All pages  created.');
+  });
+
