ea4ec0c18e734d821a9c0a660069297959f51c25
Lu Cho-Ching <choching.work@gmai.com>
2015-06-26 11:05:49 +0800

    獨立english posts 以及 index

--- conf.js
+++ conf.js
@@ -3,5 +3,6 @@ module.exports = {
   desc: '既然走過,就留下痕跡。',
   articleSource: './contents/articles/',
   pageSource: './contents/pages/',
+  englishSource: './contents/english/',
   buildto: './build/'
 };
--- gen.js
+++ gen.js
@@ -3,6 +3,7 @@ var utils = require('./utils');
 var fs = require('fs');
 var Bluebird = require('bluebird');
 Bluebird.promisifyAll(fs);
+var jade = require('jade');
 
 fs.readdirAsync(conf.articleSource)
   .then(utils.reverseDirList)
@@ -31,3 +32,42 @@ fs.readdirAsync('./contents/pages')
     console.log('[done] All pages  created.');
   });
 
+fs.readdirAsync('./contents/english')
+  .then(utils.reverseDirList)
+  //TODO: 和parseInfo只差 articleSource和 englishSource不同而已
+  .map(function(fileName){
+    var titleArr= fileName.split('.')[0].split('-');
+    var postDate = titleArr[0] +'-'+ titleArr[1] +'-'+ titleArr[2];   
+    return {
+      fileName: fileName,
+      headTitle: conf.name + ' - ' + titleArr[3],
+      postDate: postDate,
+      inPath: conf.englishSource + fileName,
+      outPath: conf.buildto + 'english/'+ postDate + '-' + titleArr[3] + '.html',
+      content: fs.readFileAsync(conf.englishSource + fileName, 'utf8')
+    };
+  })
+  .each(utils.markdownToHtml)
+  .map(function(md){
+    return md.content
+      .then(function(data){
+        return {
+          link: '/english/' + md.fileName.split('.')[0] + '.html',
+          title: data.split('\n')[0], //像是:  # 標題  
+          date: md.postDate
+        };
+      });
+  })
+  .then(function(lists){
+    var indexFn = jade.compileFile('./templates/Index.jade', {pretty:false,debug:true});
+    return indexFn({
+      source: '../', 
+      title: conf.name,
+      lists: lists
+    }); 
+  })
+  .then(fs.writeFileAsync.bind(fs, conf.buildto + 'english/index.html'))
+  .done(function(){
+    console.log('[done] english/index.html created.');
+  });
+  
